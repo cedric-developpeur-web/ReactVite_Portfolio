4d940f28208e076a5ef26c434621f685a4df8abc
cocq cedric <cedric-developpeur-web@laposte.net>
2024-10-03 23:33:05 +0200

    installation dependence traduction langue + ajoute bouton avec le langage utilisé + le nom du projet

--- front_end/src/component/competences/Skills.jsx
+++ front_end/src/component/competences/Skills.jsx
@@ -2,15 +2,14 @@ import React from 'react';
 
 const Skills = () => {
   const logoJs = '../../assets/logo_javascript.webp';
-  const logoNodeJs = '../../assets/logo_nodejs.webp';
-  const logoReact = '../../assets/logo_react.webp';
+  const logoReact = '../../assets/React_Native_Logo.webp';
   const logoVite = '../../assets/logo_vitejs.webp';
   return (
     <section id='skill'>
       <h2>Mes Compétences</h2>
       <div className='posi_skills'>
         <img src={logoJs} alt="logo javascript" />
-        <img src={logoNodeJs} alt="logo nodejs" />
+        <img src={logoReact} alt="logo nodejs" />
         <img src={logoVite} alt="logo react" />
       </div>
     </section>
--- front_end/src/component/projets/Projets.jsx
+++ front_end/src/component/projets/Projets.jsx
@@ -12,12 +12,21 @@ const Projets = () => {
       <div className='posi_projects'>
         <div>
           <img src={sophieBluel} alt="image du projet sophie bluel javascript" />
-        </div>
-        <div>
-          <img src={argentBank} alt="image du projet argent bank react avec vite" />
+          <div className='posi_button'>
+            <button>Javascript Sophie Bluel<i class="fa-solid fa-arrow-up-right-from-square"></i></button>
+          </div>
         </div>
         <div>
           <img src={kasa} alt="image du projet kasa react avec cra" />
+          <div className='posi_button'>
+            <button>React Kasa<i class="fa-solid fa-arrow-up-right-from-square"></i></button>
+          </div>
+        </div>
+        <div>
+          <img src={argentBank} alt="image du projet argent bank react avec vite" />
+          <div className='posi_button'>
+            <button>React avec Vite ArgentBank<i class="fa-solid fa-arrow-up-right-from-square"></i></button>
+          </div>
         </div>
       </div>
     </section>
