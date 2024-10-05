8ef4ce68ad8bf6ce4758c033c11e0edd11ff27a5
cocq cedric <cedric-developpeur-web@laposte.net>
2024-10-05 22:09:12 +0200

    création des fichiers json de traduction fr en + ajouter des balises img pour les drapeau + scss responsive

--- front_end/src/component/navbar/NavBar.jsx
+++ front_end/src/component/navbar/NavBar.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
 const NavBar = () => {
+  const drapeauFrance = '../../assets/france.png';
+  const drapeauAnglais = '../../assets/royaume-uni.png';
+
   return (
     <header>
       <nav>
+        <div className='posi_drapeau'>
+          <img className='drap_fr' src={drapeauFrance} alt="icone du drapeau français" />
+          <img src={drapeauAnglais} alt="icone du drapeau anglais" />
+        </div>
         <ul>
           <li><a href="#profile">Profile</a></li>
           <li><a href='#projets'>Mes Projets</a></li>
